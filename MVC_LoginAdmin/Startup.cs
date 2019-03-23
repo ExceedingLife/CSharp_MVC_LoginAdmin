7660ad1dfcb6bdf02bd26b83f7058be3fe57b6a8
Andrew Harkins <ExtremelyExceeding@gmail.com>
2019-03-23 12:58:41 -0500

    Add project files.

---
+++ MVC_LoginAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_LoginAdmin.Startup))]
+namespace MVC_LoginAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
